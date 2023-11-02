d228ef7b525f9c681d9ad025c7f9480ce0ddccc8
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-02 21:22:49 +0300

    фикс каких-то не критичных моментов

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   useEffect(() => {
     async function getData() {
-      const data = await axios.get("https://chatconnectapp.netlify.app/api/users")
-      setData(data.data)
+      const data = await axios.get(
+        "https://chatconnectapp.netlify.app/api/users",
+      );
+      setData(data.data);
     }
 
-    getData()
+    getData();
   }, []);
   return (
     <div>
-      {data.length > 0 ? (
-        data.map((user) => <div key={user.id}>{user.id}</div>)
-      ) : (
-        <div>No data available</div>
-      )}
+      {data.map((user) => (
+        <div key={user.id}>{user.username}</div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
