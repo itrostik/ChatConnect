efe7181eb2796efac95b282c881f5875388059c5
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-02 17:15:42 +0300

    удаление лишнего + проверка связки фронтенд + бэкенд

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect, useState} from "react";
+
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  useEffect(() => {
+    async function getData() {
+      const data = await axios.get("https://chatconnectapp.netlify.app/api/users")
+      setData(data.data)
+    }
 
+    getData()
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {data.length > 0 ? (
+        data.map((user) => <div key={user.id}>{user.id}</div>)
+      ) : (
+        <div>No data available</div>
+      )}
+    </div>
   )
 }
 
