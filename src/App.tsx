1db1ebc190d9f241e0802c7493f5364a0f8b8da7
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-04 22:00:08 +0300

    реализована смена темы (день/ночь) для текущих компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import Login from "./components/Login.tsx";
 import {useState} from "react";
 import Register from "./components/Register.tsx";
+import "./scss/app.scss"
+import Main from "./components/Main.tsx";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
+  const [theme, setTheme] = useState(localStorage.getItem("theme"))
+  if (!theme) {
+    setTheme("Dark")
+  }
+  if (theme === "Dark") document.body.style.background = "#000"
+  else if (theme === "Light") document.body.style.background = "#fff"
   return (
-    <div>
-      {!user ? <Login setUser={setUser}/> : ""}
-      {!user ? <Register setUser={setUser}/> : ""}
-      {user ? <div>Добро пожаловать, {user.username}</div> : ""}
+    <div className={theme === "Dark" ? "dark" : "light"}>
+      {!user ? <Login theme={theme} setUser={setUser}/> : ""}
+      {!user ? <Register theme={theme} setUser={setUser}/> : ""}
+      {user ? <Main theme={theme} setTheme={setTheme}/> : ""}
     </div>
   );
 }
