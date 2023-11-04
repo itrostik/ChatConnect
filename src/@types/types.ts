c2c66c36a547b50ccc8c955049782dfa4ef89b5e
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-04 21:57:37 +0300

    добавлен тип auth

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,7 +1,14 @@
+import React from "react";
+
 export type Inputs = {
   login: string
   password: string
   username: string,
   firstName: string,
   lastName: string
+}
+
+export type Auth = {
+  theme: string
+  setUser: React.Dispatch<any>
 }
