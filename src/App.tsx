9162c09270f003bb4dca8e164720bf4aead38faa
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-05 18:09:30 +0300

    добавляем роуты для разных компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {useState} from "react";
 import Register from "./components/Register.tsx";
 import "./scss/app.scss"
 import Main from "./components/Main.tsx";
+import Loading from "./components/Loading.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
@@ -10,13 +12,17 @@ function App() {
   if (!theme) {
     setTheme("Dark")
   }
+  const [isLoading, setIsLoading] = useState(false)
   if (theme === "Dark") document.body.style.background = "#000"
   else if (theme === "Light") document.body.style.background = "#fff"
   return (
     <div className={theme === "Dark" ? "dark" : "light"}>
-      {!user ? <Login theme={theme} setUser={setUser}/> : ""}
-      {!user ? <Register theme={theme} setUser={setUser}/> : ""}
-      {user ? <Main theme={theme} setTheme={setTheme}/> : ""}
+      <Routes>
+        <Route path="/login" element={!user && !isLoading ? <Login theme={theme} setUser={setUser} setIsLoading={setIsLoading}/> : ""}/>
+        <Route path="/registration" element={!user && !isLoading ? <Register theme={theme} setUser={setUser} setIsLoading={setIsLoading}/> : ""}/>
+        <Route path="/main" element={user ? <Main theme={theme} setTheme={setTheme}/> : ""}/>
+      </Routes>
+      {!user && isLoading ? <Loading theme={theme} /> : ""}
     </div>
   );
 }
