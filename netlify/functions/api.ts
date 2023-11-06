93ae3eb5b90db87531d403df5c4b8bcd3f5c772d
itrostik <RosASergeev@stud.kpfu.ru>
2023-11-06 15:11:02 +0300

    возможность добавлять картинки на удалённый сервер

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,18 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import usersRoute from "../../src/routes/user.route";
 import dialogsRoute from "../../src/routes/dialog.route";
 import messagesRoute from "../../src/routes/message.route";
 import serverless from "serverless-http";
-
 import cors from "cors";
+import { upload } from "../../src/utils/upload.images";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.redirect("/api");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file?.originalname}`,
+  });
+});
+
+app.use("/uploads", express.static("uploads"));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/", usersRoute);
-app.use("/api/", dialogsRoute)
-app.use("/api/", messagesRoute)
+app.use("/api/", dialogsRoute);
+app.use("/api/", messagesRoute);
 
 export const handler = serverless(app);
