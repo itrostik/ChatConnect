43129bfdca9ccb67c20936f4163f244d3e8a319c
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-02 14:38:27 +0300

    добавил тип для юзера

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,16 +1,26 @@
 import React from "react";
 
 export type Inputs = {
-  login: string
-  password: string
-  username: string,
-  firstName: string,
-  lastName: string
-}
+  avatarUrl: string;
+  login: string;
+  password: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+};
 
 export type Auth = {
-  theme: string
-  setToken: React.Dispatch<string>
-  setIsLoading: React.Dispatch<boolean>
-  token: string
-}
+  theme: string;
+  setToken: React.Dispatch<string>;
+  setIsLoading: React.Dispatch<boolean>;
+  token: string;
+};
+
+export type User = {
+  avatar: string;
+  firstName: string;
+  lastName: string;
+  login: string;
+  passwordHash: string;
+  username: string;
+};
