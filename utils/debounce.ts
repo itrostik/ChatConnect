fea415b5a0c51bf1cdc984c90d9ca45e644b55e4
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-03 17:27:23 +0300

    подрубил lodash: чёто мой debounce шалит, заменил его готовым решением

--- utils/debounce.ts
+++
@@ -1,7 +0,0 @@
-export default function debounce(callback, timeoutMs) {
-  let timeout;
-  return function (event) {
-    clearTimeout(timeout);
-    timeout = setTimeout(callback(event), timeoutMs);
-  };
-}
