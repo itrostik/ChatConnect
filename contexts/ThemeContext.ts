b3e350058338ba57c07429269b3d9b43a44c1620
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-03 21:13:02 +0300

    пробуем redux toolkit

--- contexts/ThemeContext.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Theme } from "../@types/types.ts";
-
-export const themes: Theme = {
-  dark: "dark",
-  light: "light",
-};
-
-export const ThemeContext = React.createContext(null);
