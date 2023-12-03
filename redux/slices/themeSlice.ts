188747fd0a3e5625babb4ba82d94292d72a2cb10
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-03 21:14:28 +0300

    создан глобальный стор + сделан первый slice для смены темы

---
+++ redux/slices/themeSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themes = {
+  dark: "dark",
+  light: "light",
+};
+
+const getTheme = () => {
+  const theme = `${window?.localStorage?.getItem("theme")}`;
+  if (Object.values(themes).includes(theme)) return theme;
+
+  const userMedia = window.matchMedia("(prefers-color-scheme: light)");
+  if (userMedia.matches) return themes.light;
+
+  return themes.dark;
+};
+
+const initialState = getTheme();
+
+export const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    change: (_state, action) => action.payload,
+  },
+});
+
+export const { change } = themeSlice.actions;
+
+export default themeSlice.reducer;
