1ec530d16b140baa5a385778c614147e6e5f99e3
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-05 17:00:05 +0300

    идёт плотная работа: сделал вывод всех диалогов пользователя в сайдбаре + сделал прослушивание диалогов

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,5 @@
 import styles from "./Main.module.scss";
 import { Navigate } from "react-router-dom";
-import { useState } from "react";
 import Header from "../Header/Header.tsx";
 import Sidebar from "../Sidebar/Sidebar.tsx";
 import { useSelector } from "react-redux";
@@ -11,17 +10,13 @@ export default function Main() {
   if (!localStorage.getItem("token")) {
     return <Navigate to="/login" />;
   }
-  const [activeDialog, setActiveDialog] = useState("");
   const dialog = useSelector((state: RootState) => state.dialog);
   const user = useSelector((state: RootState) => state.user);
   return (
     <div className={styles["main"]}>
       <Header user={user} />
       <div className={styles["main__content"]}>
-        <Sidebar
-          activeDialog={activeDialog}
-          setActiveDialog={setActiveDialog}
-        />
+        <Sidebar user={user} />
         {dialog.id ? (
           <Dialog dialog={dialog} user={user} />
         ) : (
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,43 +1,66 @@
 import styles from "./Sidebar.module.scss";
-import { Dispatch, SetStateAction } from "react";
+import { useEffect, useState } from "react";
+import { DialogType } from "../../../@types/dialogType.ts";
+import { MateType } from "../../../@types/mateType.ts";
+import { collection, onSnapshot, or, where, query } from "firebase/firestore";
+import db from "../../../utils/database.ts";
+// import { choose } from "../../../redux/slices/dialogSlice.ts";
+// import { useDispatch } from "react-redux";
+import { UserType } from "../../../@types/userType.ts";
+import axios from "axios";
+import { MessageType } from "../../../@types/messageType.ts";
 
-export default function Sidebar({
-  activeDialog,
-  setActiveDialog,
-}: {
-  activeDialog: string;
-  setActiveDialog: Dispatch<SetStateAction<string>>;
-}) {
+type DialogsType = DialogType & MateType;
+
+export default function Sidebar({ user }: { user: UserType }) {
+  const [dialogs, setDialogs] = useState<DialogsType[]>([]);
+  // const dispatch = useDispatch();
+  useEffect(() => {
+    const q = query(
+      collection(db, "dialogs"),
+      or(where("user_id", "==", user.id), where("user2_id", "==", user.id)),
+    );
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      const dialogs = [];
+      querySnapshot.forEach(async (doc) => {
+        const dialog = doc.data();
+        const mateId =
+          dialog.user_id === user.id ? dialog.user2_id : dialog.user_id;
+        const mate = await axios.get<UserType>(
+          `http://localhost:4444/api/users/${mateId}`,
+        );
+        dialogs.push({ ...dialog, id: doc.id, mate: mate.data });
+      });
+    });
+    setDialogs(dialogs);
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  function getLastMessage(messages: MessageType[]): MessageType {
+    return messages[messages.length - 1];
+  }
+
+  console.log(dialogs);
   return (
-    <div onClick={() => setActiveDialog("")} className={styles["sidebar"]}>
-      <div>{activeDialog}</div>
-      <div>
-        <div className={styles["dialog"]}>
-          <div className={styles["dialog__image"]}></div>
+    <div className={styles["sidebar"]}>
+      {dialogs.map((dialog) => (
+        <div key={dialog.id} className={styles["dialog"]}>
+          <div className={styles["dialog__image"]}>
+            <img src={dialog.mate.avatarUrl} alt="" width={30} />
+          </div>
           <div className={styles["dialog__info"]}>
-            <div className={styles["dialog__username"]}>Name</div>
-            <div className={styles["dialog__lastmessage"]}>LastMessage</div>
+            <div className={styles["dialog__username"]}>
+              {dialog.mate.username}
+            </div>
+            <div className={styles["dialog__lastmessage"]}>
+              {getLastMessage(dialog.messages).messageText}
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <div className={styles["dialog"]}>
-          <div className={styles["dialog__image"]}></div>
-          <div className={styles["dialog__info"]}>
-            <div className={styles["dialog__username"]}>Name</div>
-            <div className={styles["dialog__lastmessage"]}>LastMessage</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className={styles["dialog"]}>
-          <div className={styles["dialog__image"]}></div>
-          <div className={styles["dialog__info"]}>
-            <div className={styles["dialog__username"]}>Name</div>
-            <div className={styles["dialog__lastmessage"]}>LastMessage</div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
