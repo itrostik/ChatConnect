77d9b4cd63bacd173869f3e32281fed3f47c8bde
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-05 20:08:28 +0300

    написана функция для получения нормально времени сообщения

---
+++ utils/date.ts
@@ -0,0 +1,7 @@
+export function getDate(num: number) {
+  const date = new Date(num);
+  const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+  const minutes =
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+  return hours + ":" + minutes;
+}
