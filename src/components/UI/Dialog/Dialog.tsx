7fb97aba4e465800cd09158b712595001d8fd944
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-05 13:42:35 +0300

    частичная отрисовка собеседника

--- src/components/UI/Dialog/Dialog.tsx
+++ src/components/UI/Dialog/Dialog.tsx
@@ -1,5 +1,5 @@
 import styles from "./Dialog.module.scss";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { UserType } from "../../../../@types/userType.ts";
 import { DialogType } from "../../../../@types/dialogType.ts";
@@ -11,17 +11,37 @@ export default function Dialog({
   dialog: DialogType;
   user: UserType;
 }) {
+  const [mate, setMate] = useState<UserType>(null);
   useEffect(() => {
+    console.log(52);
     const getMate = async () => {
       const mateId =
         dialog.user2_id !== user.id ? dialog.user2_id : dialog.user_id;
       const mate = await axios.get<UserType>(
         `http://localhost:4444/api/users/${mateId}`,
       );
-      console.log(mate.data);
+      setMate(mate.data);
     };
     getMate();
   }, [dialog]);
 
-  return <div className={styles["dialog"]}>{dialog.id}</div>;
+  return (
+    <div className={styles["dialog"]}>
+      {mate ? (
+        <div className={styles["dialog__header"]}>
+          <div className={styles["dialog__header-image"]}>
+            <img src={mate.avatarUrl} alt="avatar" />
+          </div>
+          <div className={styles["dialog__header-info"]}>
+            <div className={styles["dialog__header-name"]}>{mate.username}</div>
+            <div className={styles["dialog__header-time"]}>
+              был в сети 5 минут назад
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
