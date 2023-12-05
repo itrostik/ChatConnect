84377da5b7233e419cf16ff99a7206d3e5adab70
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-05 16:58:37 +0300

    подправил тип, так как вынес поле messages в отдельный тип

--- @types/dialogType.ts
+++ @types/dialogType.ts
@@ -1,11 +1,8 @@
+import { MessageType } from "./messageType.ts";
+
 export type DialogType = {
   id: string;
-  messages: {
-    created: number;
-    id: string;
-    messageText: string;
-    sender_id: string;
-  }[];
+  messages: MessageType[];
   user_id: string;
   user2_id: string;
 };
