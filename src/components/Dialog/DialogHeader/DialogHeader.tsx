83a21ca50c442333fb77cb42f5f90ee62d0381e5
itrostik <RosASergeev@stud.kpfu.ru>
2023-12-10 21:53:11 +0300

    ну ничего, скоро баги закончаться...

--- src/components/Dialog/DialogHeader/DialogHeader.tsx
+++ src/components/Dialog/DialogHeader/DialogHeader.tsx
@@ -9,7 +9,7 @@ import { getDate } from "../../../../utils/date.ts";
 export default function DialogHeader({ dialog }) {
   const dispatch = useDispatch();
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "users", dialog.mate.id), (doc) => {
+    const unsub = onSnapshot(doc(db, "users", dialog.mate?.id), (doc) => {
       dispatch(
         choose({
           ...dialog,
@@ -25,16 +25,16 @@ export default function DialogHeader({ dialog }) {
   return (
     <div className={styles["dialog__header"]}>
       <div className={styles["dialog__header-image"]}>
-        <img src={dialog.mate.avatarUrl} alt="avatar" />
+        <img src={dialog.mate?.avatarUrl} alt="avatar" />
       </div>
       <div className={styles["dialog__header-info"]}>
         <div className={styles["dialog__header-name"]}>
-          {dialog.mate.username}
+          {dialog.mate?.username}
         </div>
         <div className={styles["dialog__header-time"]}>
-          {dialog.mate.online
+          {dialog.mate?.online
             ? "В сети"
-            : "Был в сети: " + getDate(dialog.mate.lastActive)}
+            : "Был в сети: " + getDate(dialog.mate?.lastActive)}
         </div>
       </div>
     </div>
